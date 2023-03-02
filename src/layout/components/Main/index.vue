22f6a2446634f7a23f640bed36a03037738b90b5
songpeng <419626398@qq.com>
2023-03-02 20:41:19 +0800

    test

---
+++ src/layout/components/Main/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({ name: "" })
+</script>
+
+<template>
+    $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/dashboard/analysis/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+defineOptions({ name: 'DashboardAnalysis' })
+</script>
+
+<template>
+    <div class="dashboardAnalysis"></div>
+</template>
+
+<style lang="less" scoped>
+.dashboardAnalysis {
+  display: grid;
+}
+</style>
